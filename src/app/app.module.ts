876a798e6e476ef6220c8129d1a39bf32ab2fdf9
ribalkoko48 <kotara22@rambler.ru>
2018-03-05 01:00:13 +0300

    header el-ts footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ElementLeftComponent } from './container/element-left/element-left.component';
+import { ElementRightComponent } from './container/element-right/element-right.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ElementLeftComponent,
+    ElementRightComponent
   ],
   imports: [
     BrowserModule
